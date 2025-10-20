513da1baa3536ae82cc936d21eff1b641e18da55
nasywanayla119-glitch <nasywanayla119@gmail.com>
2025-10-20 18:58:20 +0700

    Latihan PHP Tipedata, Variabel, Konstanta

---
+++ php/konstanta.php
@@ -0,0 +1,7 @@
+<?php
+define("NAMA", "Nasywa Nayla Azzahra");
+define("NILAI", 90);
+echo "---Belajar Konstanta --- <br><br>";
+echo "Nama : " . NAMA;
+echo "<br>Nilai : " . NILAI;
+?>
---
+++ php/variabel.php
@@ -0,0 +1,13 @@
+<?php
+$char = 'A';
+$string = "Nasywa Nayla Azzahra";
+$a = 20;
+$penghasilan1 = 1000000;
+$nilai_uts = 82.25;
+echo "---Belajar Variabel --- <br><br>";
+echo "Variabel Char : " . $char . "<br>";
+echo "Variabel String : $string<br>";
+echo "Variabel a : $a<br>";
+echo "Variabel penghasilan1 : $penghasilan1<br>";
+echo "Variabel nilai_uts : $nilai_uts<br>";
+?>
