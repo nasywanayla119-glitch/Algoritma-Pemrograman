f50ce5d0eb6f89e52a79e7247932388c1cfc58b1
nasywanayla119-glitch <nasywanayla119@gmail.com>
2025-10-20 18:55:37 +0700

    Belajar dan latihan Java tipe data, integer, dll

---
+++ java/local.java
@@ -0,0 +1,6 @@
+public class local {
+    public static void main(String args[]) {
+        int age = 20; // local variable
+        System.out.println("Umur Nasywa Nayla Azzahra : " + age);
+    }
+}
