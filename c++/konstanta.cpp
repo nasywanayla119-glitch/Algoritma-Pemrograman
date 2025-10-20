cc4dd30cbc6ced88b54e2528f52c5c670f0fb828
nasywanayla119-glitch <nasywanayla119@gmail.com>
2025-10-20 18:53:18 +0700

    Latihan C++ Tipe data, Variabel, Konstanta

---
+++ c++/konstanta.cpp
@@ -0,0 +1,20 @@
+#include <conio.h>
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+main()
+{
+    const double PI = 3.14;
+    const int NILAI_MAX = 100;
+    const char MyChar = 'A';
+    const string MyName = "Nasywa Nayla Azzahra";
+
+    cout << "---BELAJAR KONSTANTA---" << endl;
+    cout << "Konstanta PI : " << PI << endl;
+    cout << "Konstanta NILAI_MAX : " << NILAI_MAX << endl;
+    cout << "Konstanta MyChar : " << MyChar << endl;
+    cout << "Konstanta MyName : " << MyName << endl;
+    cout << endl;
+}
